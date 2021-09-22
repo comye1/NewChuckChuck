fa328af3360115c626959e9a6426fb33a727b4af
comye1 <comye1@seoultech.ac.kr>
2021-09-22 23:27:59 +0900

    firebase auth 작업중

--- app/src/main/java/com/comye1/keywordnote/MainActivity.kt
+++ app/src/main/java/com/comye1/keywordnote/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.comye1.keywordnote
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,12 +12,33 @@ import com.comye1.keywordnote.navigation.Navigation
 import com.comye1.keywordnote.ui.theme.DeepGreen
 import com.comye1.keywordnote.ui.theme.KeywordNoteTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.io.IOException
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         setContent {
             KeywordNoteTheme() {
@@ -31,9 +51,16 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        val bmp = BitmapFactory.decodeResource(resources, R.drawable.cat)
-        savePhotoToInternalStorage("sampleImage5", bmp)
+//        val bmp = BitmapFactory.decodeResource(resources, R.drawable.cat)
+//        savePhotoToInternalStorage("sampleImage5", bmp)
 //        deletePhotoFromInternalStorage("sampleImage.jpg")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        var currentUser = auth.getCurrentUser()
+//        updateUI(currentUser)
     }
 
     fun savePhotoToInternalStorage(filename: String, bmp: Bitmap): Boolean {
